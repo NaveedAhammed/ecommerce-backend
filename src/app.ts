b1da20a2d9c6322f8159cd064e7a51ed592f24b6
NaveedAhammed <naveedahammed549@gmail.com>
2024-02-16 20:30:00 +0530

    initial setup

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+import { connect } from "./utils/database.js";
+
+const PORT: string | undefined = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
